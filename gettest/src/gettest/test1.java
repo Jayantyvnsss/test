28547bc5ed60bc619759bf3b74395d6a9365f95e
Jay <Jay@192.168.0.9>
2017-12-25 17:44:10 +0530

    Add loop jay for 10 times

--- gettest/src/gettest/test1.java
+++ gettest/src/gettest/test1.java
@@ -5,6 +5,12 @@ public class test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
                System.out.println("x");
+               
+               
+               for(int i=0;i<10;i++)
+               {
+            	   System.out.println("jay");
+               }
 	}
 
 }
